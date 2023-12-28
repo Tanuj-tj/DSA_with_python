4e4535cd061e7c4625f1b8923cdc4f8e4d4ef696
Tanuj shrivastava <63875409+Tanuj-tj@users.noreply.github.com>
2023-12-28 23:17:53 +0530

    Update easy.py, added majority elements problem

--- problems/array/easy.py
+++ problems/array/easy.py
@@ -49,3 +49,18 @@ class ArrayEasy:
                 nums[k] = nums[i]
                 k += 1
         return k
+
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 1
+        res = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] == res:
+                count += 1
+            else:
+                count -= 1
+                if count < 0:
+                    count = 0
+                    res = nums[i]
+
+        return res
